6c37c7a779b3fc88c6011aba608bf6bcef34c207
ysk-9 <yaswanthsaikiran9@gmail.com>
2022-05-03 15:55:52 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+function changeMode(){
+    let mybody = document.body;
+    mybody.classList.toggle("mydark");
+}
+function loadCoupon(){
+    document.getElementById("coupon").style.display="block"
+
+}
+function closeCoupon(){
+    document.getElementById("coupon").style.display="none"
+    
+}
