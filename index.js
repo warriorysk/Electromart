ebeb2d3d73298df483d66bd9e5bd158b8250979c
ysk-9 <yaswanthsaikiran9@gmail.com>
2022-05-10 20:35:53 +0530

    first commit

--- index.js
+++ index.js
@@ -10,30 +10,17 @@ function closeCoupon(){
     document.getElementById("coupon").style.display="none"
     
 } 
-let x = document.getElementById('out');
-function geolocation(){
-    if(navigator.geolocation){
-        navigator.geolocation.getCurrentPosition(showPosition)
-    }else{
-        x.innerText="Geo Not Supported"
-    }
-}
-
-function showPosition(data){
-    console.log(data)
-    sessionStorage.setItem('data',data)
-    let latitude = data.coords.latitude;
-    let longitude = data.coords.longitude
-    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
-
-    /// api Calling 
-    fetch(url,{method:'GET'})
-    // return promise
-    .then((res) => res.json())
-    // return data
-    .then((data) => {
-        let cityName = data.city.name
-        let weather = data.list[0].temp.day
-        x.innerText = `Weather of ${cityName} is ${weather} °C`
-    })
+navigator.geolocation.getCurrentPosition(position);
+async function position(data) {
+    const display = document.getElementsByClassName('temp-display');
+    const logo = document.getElementById('imG');
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${data.coords.latitude}&lon=${data.coords.longitude}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+    let response = await fetch(`${url}`);
+    let info = await response.json();
+ console.log(info);
+    display[0].innerHTML = info.list[0].temp.day;
+    var iconcode = info.list[0].weather[0].icon;
+    var iconurl = "http://openweathermap.org/img/w/" + iconcode + ".png";
+    logo.src = iconurl;
+    display[1].innerHTML = info.city.name;
 }
