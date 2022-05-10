552fcd2b93e955960282423c2415db4bd91cae64
ysk-9 <yaswanthsaikiran9@gmail.com>
2022-05-10 15:32:37 +0530

    first commit

--- index.js
+++ index.js
@@ -9,4 +9,31 @@ function loadCoupon(){
 function closeCoupon(){
     document.getElementById("coupon").style.display="none"
     
+} 
+let x = document.getElementById('out');
+function geolocation(){
+    if(navigator.geolocation){
+        navigator.geolocation.getCurrentPosition(showPosition)
+    }else{
+        x.innerText="Geo Not Supported"
+    }
+}
+
+function showPosition(data){
+    console.log(data)
+    sessionStorage.setItem('data',data)
+    let latitude = data.coords.latitude;
+    let longitude = data.coords.longitude
+    const url = `https://api.openweathermap.org/data/2.5/forecast/daily?lat=${latitude}&lon=${longitude}&mode=json&units=metric&cnt=1&appid=fbf712a5a83d7305c3cda4ca8fe7ef29`;
+
+    /// api Calling 
+    fetch(url,{method:'GET'})
+    // return promise
+    .then((res) => res.json())
+    // return data
+    .then((data) => {
+        let cityName = data.city.name
+        let weather = data.list[0].temp.day
+        x.innerText = `Weather of ${cityName} is ${weather} °C`
+    })
 }
